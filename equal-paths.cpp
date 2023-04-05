6901a78a30190a8588b810baf5662c8b2978cd3a
Taryn Sanders <tosander@usc.edu>
2023-04-05 17:20:23 +0000

    Submission Attempt 5

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,5 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include <iostream>
 
 #endif
 
@@ -8,11 +9,56 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int* heightCounter(Node * node, char direction, int* depths);
 
-
-bool equalPaths(Node * root)
+int* heightCounter(Node * node, char direction, int* depths)
 {
-    // Add your code below
+    if(node == NULL){
+      return depths;
+    }
+    if(node->left != NULL || node->right != NULL){
+      if(direction == 'l' ){
+        depths[0] += 1;
+        heightCounter(node->left, 'l', depths);
+        heightCounter(node->right, 'l', depths);
+      }
+      else if(direction == 'r' ){
+        depths[1] += 1;
+        heightCounter(node->right, 'r', depths);
+        heightCounter(node->left, 'r', depths);
+      }
+      else if(direction == 'x' && (node->right != NULL || node->left != NULL)){
+        depths[0]+= 1;
+        depths[1]+= 1;
+        heightCounter(node->right, 'r', depths);
+        heightCounter(node->left, 'l', depths);
+      }
+
+    }
+    
+    return depths;
 
 }
+  
+bool equalPaths(Node * root)
+{
+  
+    int* heights = new int[2]{0, 0};
+    int* newHeights;
+
+    newHeights = heightCounter(root, 'x', heights);
+
+    if(root != NULL && ((root->left != NULL && root->right != NULL) || (root->left != NULL || root->right != NULL))){
+      if(newHeights[1] != newHeights[0]){
+        delete[] heights;
+        return false;
+      }
+    }
+    
+    delete[] heights;
+
+    return true;
+
+}
+
 
